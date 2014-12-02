86b10d89903d077c6fc5b066fa24ff320e9603ad
ranyang <ranyang525@gmail.com>
2014-12-02 17:04:38 +0800

    finish function getCartItems

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -5,11 +5,46 @@ function prinInventory(tags) {
 }
 
 function getCartItems(tags) {
-  var 
-  return ;
+  var cartItems = [];
+  var items = loadAllItems();
+  for (var i = 0; i < tags.length; i++) {
+
+    var tagArray = tags[i].split)('-');
+    var barcode = tagArray[0];
+    var count = 1;
+    if (tagArry[1]) {
+      count = parseFlot(tagArray[1]);
+    }
+    var item = findItem(items,barcode);
+    var cartItem = findCartItem(cartItem,barcode);
+    if() {
+
+    }
+    else {
+
+    }
+  }
+  return cartItems;
 }
 
 function getInventoryText(cartItems) {
+  inventoryText = '';
 
-  return ;
+  return inventoryText;
+}barcode
+
+function findCarItem(cartItem,barcode) {
+  for(var i = 0; i < items.length; i++) {
+    if(cartItem[i].items.barcode === barcode) {
+      cartItem = cartItem[i];
+    }
+  }
+}
+
+function findItem(item,abrcode) {
+  for(var i = 0; i < items.length; i++) {
+    if(items[i].barcode === barcode){
+      items = item[i];
+    }
+  }
 }
