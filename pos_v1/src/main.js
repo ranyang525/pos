ac5af7a3f48e16c6c25e65e39a10fb1f1f8ea3a6
ranyang <ranyang525@gmail.com>
2014-12-02 15:34:15 +0800

    add function

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -1,1 +1,15 @@
-//TODO: Please write code in this file.
+function prinInventory(tags) {
+  var cartItems = getCartItems(tags);
+  var inventoryText = getInventoryText(cartItems);
+  console.log(inventoryText);
+}
+
+function getCartItems(tags) {
+  var 
+  return ;
+}
+
+function getInventoryText(cartItems) {
+
+  return ;
+}
